2fd8052401b16116035ab536d0fd7af8df93a678
RedBlueBird <jiefu.zhang1226@gmail.com>
2023-02-14 23:41:47 -0800

    commit made just to debug the linux

--- main.py
+++ main.py
@@ -116,6 +116,7 @@ class AdventurerBot(commands.Bot):
 
     async def setup_hook(self):
         print("setting up cogs?")
+        print(walk_modules("cogs"))
         for ext in walk_modules("cogs"):
             print("ok there are at least cogs being found")
             name = ext.__name__
