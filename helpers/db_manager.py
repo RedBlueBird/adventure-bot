4c3622742029197aa68f9ca6745678d184c9016f
Eric Chang <55172722+echang594@users.noreply.github.com>
2023-02-15 22:04:41 -0800

    random parenthesis

--- helpers/db_manager.py
+++ helpers/db_manager.py
@@ -43,7 +43,7 @@ def init():
 
 
 def is_registered(uid: int) -> bool:
-    cur.execute(f"SELECT EXISTS(SELECT * FROM temp WHERE userid = {uid})")
+    cur.execute(f"SELECT EXISTS(SELECT * FROM temp WHERE userid = {uid}")
     return bool(cur.fetchall()[0][0])
 
 
