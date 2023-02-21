e7590e98e2ff92015f00bd27a35e200795329dc5
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2023-02-20 20:46:17 -0800

    idt t.final is necessary

--- util/constants.py
+++ util/constants.py
@@ -1,35 +1,34 @@
 import json
-import typing as t
 import os
 
 dirname = os.path.join(os.path.dirname(__file__), "..", "resources/text")
 
-PREF: t.Final = "a."
-SCALE: t.Final = 50, 1.05
+PREF = "a."
+SCALE = 50, 1.05
 
-DECK_LVL_REQ: t.Final = {1: 0, 2: 0, 3: 6, 4: 15, 5: 21, 6: 29}
+DECK_LVL_REQ = {1: 0, 2: 0, 3: 6, 4: 15, 5: 21, 6: 29}
 MAX_CARDS = 500
 BP_CAP = 100
 
 # Loads in all the necessary JSON files
 with open(os.path.join(dirname, "icons.json")) as json_file:
-    ICON: t.Final = json.load(json_file)
+    ICON = json.load(json_file)
 with open(os.path.join(dirname, "admins.json")) as json_file:
-    ADMINS: t.Final = set(json.load(json_file))
+    ADMINS = set(json.load(json_file))
 with open(os.path.join(dirname, "cards.json")) as json_file:
-    CARDS: t.Final = json.load(json_file)
+    CARDS = json.load(json_file)
 with open(os.path.join(dirname, "item_abbreviations.json")) as json_file:
-    ITEM_ABB: t.Final = json.load(json_file)
+    ITEM_ABB = json.load(json_file)
 with open(os.path.join(dirname, "items.json")) as json_file:
-    ITEMS: t.Final = json.load(json_file)
+    ITEMS = json.load(json_file)
 with open(os.path.join(dirname, "mobs.json")) as json_file:
-    MOBS: t.Final = json.load(json_file)
+    MOBS = json.load(json_file)
 with open(os.path.join(dirname, "effects.json")) as json_file:
-    EFFX: t.Final = json.load(json_file)
+    EFFX = json.load(json_file)
 with open(os.path.join(dirname, "card_list.json")) as json_file:
-    CARD_LIST: t.Final = json.load(json_file)
+    CARD_LIST = json.load(json_file)
 with open(os.path.join(dirname, "levels.json")) as json_file:
-    LEVELS: t.Final = json.load(json_file)
+    LEVELS = json.load(json_file)
 with open(os.path.join(dirname, "hometown.json")) as json_file:
     HTOWN = json.load(json_file)
 with open(os.path.join(dirname, "adventure.json")) as json_file:
@@ -44,7 +43,7 @@ with open(os.path.join(dirname, "tutorial.json")) as json_file:
         object_pairs_hook=lambda x: {int(k) if k.isdigit() else k: v for k, v in x},
     )
 
-CONVERT: t.Final = {
+CONVERT = {
     "burn": ICON["burn"],
     "poison": ICON["pois"],
     "recover": ICON["rec"],
