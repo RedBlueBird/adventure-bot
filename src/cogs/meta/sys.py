56dbe920928557901d75a5f3a27dff8751a8cf95
Kevin Sheng <kevinsheng2005@gmail.com>
2024-03-20 21:44:54 -0700

    split up the `on_command` function

--- src/cogs/meta/sys.py
+++ src/cogs/meta/sys.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 import db
-from helpers import util as u, resources as r, db_manager as dm
+from helpers import util as u, resources as r
 
 
 class Sys(commands.Cog):
@@ -60,100 +60,112 @@ class Sys(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command(self, ctx: Context):
-        if ctx.bot:
+        if ctx.author.bot:
             return
 
-        # region Check user level up
-        a = ctx.author
-        lvl = dm.get_user_level(a.id)
-        if not lvl:
-            return
-        xp = dm.get_user_exp(a.id)
-
-        if xp >= u.level_xp(lvl) and lvl < 30:
-            level_msg = []
-            if (lvl + 1) % 2 == 0:
-                add_hp = round(
-                    (r.SCALE[1] ** math.floor((lvl + 1) / 2) - r.SCALE[1] ** math.floor(lvl / 2))
-                    * 100
-                    * r.SCALE[0]
-                )
-                level_msg.append(f"Max health +{add_hp}!")
-
-            # At levels 17 and 27, the user gets a week of free premium.
-            if lvl + 1 in [17, 27]:
-                dm.set_user_premium(a.id, dt.datetime.now(dt.timezone.utc) + dt.timedelta(days=7))
-
-            if r.LEVELS[lvl - 1]:
-                level_msg.extend(r.LEVELS[lvl - 1].format(r.PREF).split("\n"))
-
-            embed = discord.Embed(
-                title=f"Congratulations {a.name}!",
-                description=None,
-                color=discord.Color.green(),
-            )
-
-            coin_gain = lvl * 50
-            gem_gain = math.ceil((lvl + 1) / 5) + 1
-            embed.add_field(
-                name=f"You're now level {lvl + 1}!",
-                value=f"+{coin_gain} {r.ICONS['coin']} \n+{gem_gain} {r.ICONS['gem']} \n```» "
-                + "\n\n» ".join(level_msg)
-                + "```",
-            )
-            embed.set_thumbnail(url=a.avatar.url)
-            await ctx.reply(embed=embed)
-
-            dm.set_user_exp(a.id, xp - u.level_xp(lvl))
-            dm.set_user_level(a.id, lvl + 1)
-            dm.set_user_coin(a.id, dm.get_user_coin(a.id) + coin_gain)
-            dm.set_user_coin(a.id, dm.get_user_gem(a.id) + gem_gain)
-        # endregion
+        await self.check_levels(ctx)
 
         # region Quest Completion Check (temporary)
-        quests = dm.get_user_quests(a.id)
-        for quest in quests:
-            await u.update_quest(ctx, a.id, quest[1], 0)
+        # quests = dm.get_user_quests(a.id)
+        # for quest in quests:
+        #     await u.update_quest(ctx, a.id, quest[1], 0)
         # endregion
 
-        # region Gold Spawn Logic
-        if random.randint(1, 25) == 1:
-            if random.randint(1, 30) == 1:
-                amt = random.randint(250, 500)
-            else:
-                amt = random.randint(50, 100)
+        if random.randint(1, 25) != 1:
+            await self.spawn_coin(ctx)
 
-            await ctx.channel.send(
-                embed=discord.Embed(
-                    title=f"A bag of gold showed up out of nowhere!",
-                    description=(
-                        f"Quick! Type `{r.PREF}collect {amt} coins` to collect them!\n"
-                        "They'll be gone in 10 minutes!"
-                    ),
-                    color=discord.Color.green(),
-                )
+    async def check_levels(self, ctx: Context):
+        a = ctx.author
+        player = db.Player.get_or_none(db.Player.id == a.id)
+        if player is None:
+            return
+
+        lvl = player.level
+        needed_xp = u.level_xp(lvl)
+        if player.xp < needed_xp or player.level >= 30:
+            return
+
+        level_msg = []
+        if (lvl + 1) % 2 == 0:
+            add_hp = round(
+                (r.SCALE[1] ** math.floor((lvl + 1) / 2) - r.SCALE[1] ** math.floor(lvl / 2))
+                * 100
+                * r.SCALE[0]
             )
-            try:
-                rep: discord.Message = await self.bot.wait_for(
-                    "message",
-                    timeout=600.0,
-                    check=lambda m: m.content.lower() == f"{r.PREF}collect {amt} coins",
-                )
-                user_coin = dm.get_user_coin(a.id)
-                if user_coin:
-                    dm.set_user_coin(a.id, user_coin + amt)
-                    if random.randint(1, 100) == 1:
-                        dm.set_user_gem(a.id, dm.get_user_gem(a.id) + 1)
-                        msg = f"You got {amt} {r.ICONS['coin']} and a bonus {r.ICONS['gem']}!"
-                    else:
-                        msg = f"You got {amt} {r.ICONS['coin']}!"
-                else:
-                    msg = f"You have to register in this bot first with `{r.PREF}register`!"
+            level_msg.append(f"Max health +{add_hp}!")
 
-                await rep.reply(msg)
-            except asyncio.TimeoutError:
-                print("No one claimed the bag of coins.")
-        # endregion
+        # At levels 17 and 27, the user gets a week of free premium.
+        if lvl + 1 in [17, 27]:
+            player.premium_acc = dt.date.today() + dt.timedelta(days=7)
+
+        if r.LEVELS[lvl - 1]:
+            level_msg.extend(r.LEVELS[lvl - 1].format(r.PREF).split("\n"))
+
+        embed = discord.Embed(
+            title=f"Congratulations {a.name}!",
+            description=None,
+            color=discord.Color.green(),
+        )
+
+        coin_gain = lvl * 50
+        gem_gain = math.ceil((lvl + 1) / 5) + 1
+        embed.add_field(
+            name=f"You're now level {lvl + 1}!",
+            value=f"+{coin_gain} {r.ICONS['coin']} \n+{gem_gain} {r.ICONS['gem']} \n```» "
+                  + "\n\n» ".join(level_msg)
+                  + "```",
+        )
+        embed.set_thumbnail(url=a.avatar.url)
+        await ctx.reply(embed=embed)
+
+        player.xp -= needed_xp
+        player.level += 1
+        player.coins += coin_gain
+        player.gems += gem_gain
+        player.save()
+
+    async def spawn_coin(self, ctx: Context):
+        if random.randint(1, 30) == 1:
+            lo, hi = 250, 500
+        else:
+            lo, hi = 50, 100
+        amt = random.randint(lo, hi)
+
+        msg = await ctx.channel.send(
+            embed=discord.Embed(
+                title=f"A bag of gold showed up out of nowhere!",
+                description=(
+                    f"Quick! Type `{r.PREF}collect {amt} coins` to collect them!\n"
+                    "They'll be gone in 10 minutes!"
+                ),
+                color=discord.Color.green(),
+            )
+        )
+        try:
+            rep: discord.Message = await self.bot.wait_for(
+                "message",
+                timeout=600.0,
+                check=lambda m: m.content.lower() == f"{r.PREF}collect {amt} coins",
+            )
+        except asyncio.TimeoutError:
+            await msg.edit(content="No one claimed the bag of coins.")
+            return
+
+        give_to = rep.author.id
+        claimer = db.Player.get_or_none(db.Player.id == give_to)
+        if claimer is None:
+            await rep.reply(f"You have to register in this bot first with `{r.PREF}register`!")
+            return
+
+        claimer.coins += amt
+        if random.randint(1, 100) == 1:
+            claimer.gems += 1
+            msg = f"You got {amt} {r.ICONS['coin']} and a bonus {r.ICONS['gem']}!"
+        else:
+            msg = f"You got {amt} {r.ICONS['coin']}!"
+
+        claimer.save()
+        await rep.reply(msg)
 
 
 async def setup(bot: commands.Bot):
