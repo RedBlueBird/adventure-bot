846da40bb9720f0360ae694df4ca17bcd2bd0b5d
Kevin Sheng <kevinsheng2005@gmail.com>
2024-03-23 21:36:43 -0700

    move the locking functinos to their own file

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,25 +1,4 @@
 from .models import Player, Deck, Card, DeckCard, Quest, Deal
 from .types import QuestType, QuestRarity, RewardType
 from .util import get_deck, sort_cards
-from exceptions import UserPreoccupied
-
-_actions: dict[int, tuple[str, str]] = {}
-
-
-def lock_user(uid: int, cmd: str, action: str):
-    if uid in _actions:
-        raise UserPreoccupied(_actions[uid][1])
-    _actions[uid] = cmd, action
-
-
-def get_user_action(uid: int):
-    return _actions[uid][1] if uid in _actions else None
-
-
-def change_user_action(uid: int, new_action: str):
-    _actions[uid] = _actions[uid][0], new_action
-
-
-def unlock_user(uid: int, cmd: str):
-    if uid in _actions and _actions[uid][0] == cmd:
-        del _actions[uid]
+from .lock import lock_user, get_user_action, change_user_action, unlock_user
---
+++ src/db/lock.py
@@ -0,0 +1,22 @@
+from exceptions import UserPreoccupied
+
+_actions: dict[int, tuple[str, str]] = {}
+
+
+def lock_user(uid: int, cmd: str, action: str):
+    if uid in _actions:
+        raise UserPreoccupied(_actions[uid][1])
+    _actions[uid] = cmd, action
+
+
+def get_user_action(uid: int):
+    return _actions[uid][1] if uid in _actions else None
+
+
+def change_user_action(uid: int, new_action: str):
+    _actions[uid] = _actions[uid][0], new_action
+
+
+def unlock_user(uid: int, cmd: str):
+    if uid in _actions and _actions[uid][0] == cmd:
+        del _actions[uid]
